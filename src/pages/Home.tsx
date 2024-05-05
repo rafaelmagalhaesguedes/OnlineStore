2ff422c4dfeb8f74c34cbbe66692597092dbad32
rafael <rafaelmagalhaes1@live.com>
2024-05-05 20:52:22 -0300

    feat: Home page created

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,18 @@
 import { Row, Col } from 'react-bootstrap';
+import { productsArray } from '../utils/productStore';
+import { ProductCard } from '../components/ProductCard';
 
 export function Home() {
   return (
-    <Row>
-      <Col>
-        <h1>Home</h1>
-        <p>
-          <a href="/success">Success</a>
-        </p>
-        <p>
-          <a href="/cancel">Cancel</a>
-        </p>
-      </Col>
-    </Row>
+    <>
+      <h1 className="p-3">Welcome to the store!</h1>
+      <Row xs={ 1 } md={ 2 } lg={ 3 } className="g-4">
+        {productsArray.map((product, idx) => (
+          <Col align="center" key={ idx }>
+            <ProductCard product={ product } />
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
