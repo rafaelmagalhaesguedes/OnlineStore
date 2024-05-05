5b999a5a71be07c829be29b81c32a9a3f7b7801b
rafael <rafaelmagalhaes1@live.com>
2024-05-05 20:51:03 -0300

    feat: productStore db created

--- src/utils/productStore.ts
+++ src/utils/productStore.ts
@@ -3,21 +3,21 @@ import { ProductType } from "../types/ProductType";
 const productsArray: ProductType[] = [
   {
     id: "1",
-    title: "Xiomi Redmi Note 10 Pro",
+    name: "Xiomi Redmi Note 10 Pro",
     price: 300.99,
-    image: "https://via.placeholder.com/150"
+    image: "https://http2.mlstatic.com/D_NQ_NP_633982-MLA46168332992_052021-O.webp"
   },
   {
     id: "2",
-    title: "Iphone 12 Pro Max",
+    name: "Iphone 12 Pro Max",
     price: 599.99,
-    image: "https://via.placeholder.com/150"
+    image: "https://http2.mlstatic.com/D_NQ_NP_760492-MLU69496071589_052023-O.webp"
   },
   {
     id: "3",
-    title: "Samsung Galaxy S21 Ultra",
+    name: "Samsung Galaxy S21 Ultra",
     price: 399.99,
-    image: "https://via.placeholder.com/150"
+    image: "https://http2.mlstatic.com/D_NQ_NP_950192-MLA74242183335_012024-O.webp"
   }
 ];
 
