4e737841ec87a68a6dae351eb7a2311dd9f34af6
rafael <rafaelmagalhaes1@live.com>
2024-05-05 20:52:42 -0300

    feat: install boostrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CartProvider from './context/CartProvider';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
-import { Success } from "./pages/Success";
-import { Cancel } from "./pages/Cancel";
-import { Home } from "./pages/Home";
+import { Success } from './pages/Success';
+import { Cancel } from './pages/Cancel';
+import { Home } from './pages/Home';
 
 function App() {
-
   return (
-    <Container>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/success" element={<Success />} />
-          <Route path="/cancel" element={<Cancel />} />
-        </Routes>
-      
-      </BrowserRouter>
-    </Container>
+    <CartProvider>
+      <Container>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/cancel" element={<Cancel />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </CartProvider>
   )
 }
 
-export default App
+export default App;
