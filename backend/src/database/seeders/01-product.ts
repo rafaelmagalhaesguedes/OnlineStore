b2d099724d39b65007fcae0a27fca6447b815e5f
rafael <rafaelmagalhaes1@live.com>
2024-05-07 14:00:39 -0300

    refact: seeders products

--- backend/src/database/seeders/01-product.ts
+++ backend/src/database/seeders/01-product.ts
@@ -4,25 +4,25 @@ export default {
   up: async (queryInterface: QueryInterface) => {
     await queryInterface.bulkInsert('products', [
       {
-        name: 'Iphone 23',
-        price: '500',
-        price_id: 'price_1J2e2b2eZvKYlo2C',
+        name: 'iPhone 14',
+        price: '999.99',
+        price_id: 'price_1PDRSIGl8SYs9Zeb0Cd0FeTI',
         quantity: 10,
-        image: 'https://via.placeholder.com/150',
+        image: 'https://http2.mlstatic.com/D_NQ_NP_881016-MLM51559383738_092022-O.webp',
       },
       {
-        name: 'Samsung Galaxy 30',
-        price: '400',
-        price_id: 'price_1J2e2b2eZvKYlo2D',
+        name: 'Samsung Galaxy A54',
+        price: '699.99',
+        price_id: 'price_1PDR05Gl8SYs9ZebgUTbavi3',
         quantity: 15,
-        image: 'https://via.placeholder.com/150',
+        image: 'https://http2.mlstatic.com/D_NQ_NP_764911-MLA70635682917_072023-O.webp',
       },
       {
-        name: 'Xiaomi Redmi 10',
-        price: '300',
-        price_id: 'price_1J2e2b2eZvKYlo2E',
+        name: 'Xiaomi Redmi 12',
+        price: '399.99',
+        price_id: 'price_1PDR0VGl8SYs9Zeb8njkL6BL',
         quantity: 20,
-        image: 'https://via.placeholder.com/150',
+        image: 'https://http2.mlstatic.com/D_NQ_NP_778864-MLA70971122056_082023-O.webp',
       }
     ], {});
   },
