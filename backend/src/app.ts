e85864906c0a1ac6a75426a25cf63f53d0a96910
rafael <rafaelmagalhaes1@live.com>
2024-05-07 12:39:26 -0300

    refact: app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,46 +1,18 @@
 import * as express from 'express';
-import { Request, Response } from 'express';
 import * as cors from 'cors';
-import Stripe from 'stripe';
 import router from './routes';
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
-const stripe = new Stripe(process.env.STRIPE_SECRET as string);
-
 app.get('/', (_req, res) => {
-  res.send('Hello World!');
+  res.send('Health check ok');
 });
 
-app.post('/checkout', async (req: Request, res: Response) => {
-  const items = req.body.items;
-
-  let lineItems: { price: string; quantity: number }[] = [];
-
-  items.forEach((item: any)=> {
-    lineItems.push(
-      {
-        price: item.id,
-        quantity: item.quantity
-      }
-    )
-  });
-
-  const session = await stripe.checkout.sessions.create({
-    line_items: lineItems,
-    mode: 'payment',
-    success_url: "http://localhost:5173/success",
-    cancel_url: "http://localhost:5173/cancel"
-  });
-
-  res.send(JSON.stringify({
-    url: session.url
-  }));
-});
-
-app.listen(3001, () => {
-  console.log('Server is running on http://localhost:3001');
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
