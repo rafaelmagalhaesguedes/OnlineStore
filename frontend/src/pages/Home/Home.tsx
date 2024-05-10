d15b3e95bfdff244dcbdc7206f4da1e498d6bff4
rafael <rafaelmagalhaes1@live.com>
2024-05-10 18:26:59 -0300

    refact: styles home page

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,29 @@
+import { useContext, useEffect } from 'react';
+import { ProductCard } from '../../components/ProductCard';
+import productService from '../../services/ApiService';
+import { CartContext } from '../../context/CartContext';
+import { Card, CardProducts, HomeContainer } from './Style';
+
+export function Home() {
+  const { products, setProducts } = useContext(CartContext);
+
+  useEffect(() => {
+    const handleProducts = async () => {
+      const response = await productService.getProducts();
+      setProducts(response);
+    };
+    handleProducts();
+  }, [setProducts]);
+
+  return (
+    <HomeContainer>
+      <CardProducts>
+        {products && products.map((product) => (
+          <Card key={ product.id }>
+            <ProductCard product={ product } />
+          </Card>
+        ))}
+      </CardProducts>
+    </HomeContainer>
+  );
+}
---
+++ frontend/src/pages/Home/Style.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: 2rem 0;
+
+  width: 80%;
+  height: 100%;
+`;
+
+export const CardProducts = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 1rem;
+
+  width: 100%;
+`;
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  padding: 1rem;
+  border-radius: 8px;
+  background-color: #fff;
+`;
