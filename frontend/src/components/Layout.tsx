dd1ee39e0a0875f6d5baa63451fadd0c2bb92e6b
rafael <rafaelmagalhaes1@live.com>
2024-05-10 18:26:47 -0300

    feat: Layout component created

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { Outlet } from 'react-router-dom';
+import { Header } from './Header';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export function Layout() {
+  return (
+    <Container>
+      <Header />
+      <Outlet />
+    </Container>
+  );
+}
