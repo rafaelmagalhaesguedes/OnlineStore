9e29041bac4b678ea9a725f9eb850f6044352ea9
rafael <rafaelmagalhaes1@live.com>
2024-05-10 19:30:25 -0300

    refact: header component

---
+++ frontend/src/components/Header/Style.tsx
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 80px;
+
+  background-color: #000;
+`;
+
+export const HeaderNavbar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+    
+  width: 80%;
+    
+  padding: 0 1rem;
+`;
+
+export const NavbarBrand = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  
+  img {
+    width: 50px;
+    height: 50px;
+  }
+
+  h2 {
+    padding-top: 10px;
+    color: white;
+    font-weight: 700;
+  }
+`;
+
+export const NavbarCollapse = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem
+`;
+
+export const NavbarText = styled.div`
+  a {
+    font-weight: 500;
+    color: white;
+    margin-left: 1rem;
+    text-decoration: none;
+  }
+`;
+
+export const CardButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: transparent;
+  border: none;
+
+  cursor: pointer;
+
+  svg {
+    color: white;
+  }
+
+  span {
+    color: white;
+    font-weight: 500;
+
+    position: relative;
+    top: -1rem;
+  }
+`;
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { Outlet } from 'react-router-dom';
-import { Header } from './Header';
+import { Header } from '../components/Header/Header';
 
 const Container = styled.div`
   display: flex;
