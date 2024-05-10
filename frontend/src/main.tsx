4da58210840154843c0fe8ab8fc262b9e33f7fa1
rafael <rafaelmagalhaes1@live.com>
2024-05-10 18:26:34 -0300

    refact: app and main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import App from './App.tsx';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import CartProvider from './context/CartProvider.tsx';
 import { GlobalStyle } from './assets/css/GlobalStyle.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GlobalStyle />
-    <App />
-  </React.StrictMode>,
-)
+  <CartProvider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <GlobalStyle />
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </CartProvider>,
+);
