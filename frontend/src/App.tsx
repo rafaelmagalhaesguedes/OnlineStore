9a69ec1170cf980d2fa57ec92213d375ec758896
rafael <rafaelmagalhaes1@live.com>
2024-05-11 08:39:47 -0300

    feat: page products details created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { Success } from './pages/Success';
 import { Cancel } from './pages/Cancel';
 import { Home } from './pages/Home/Home';
 import { Layout } from './components/Layout';
+import { ProductDetails } from './pages/ProductDetails/ProductDetails';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Layout /> }>
         <Route index element={<Home />} />
+        <Route path="/product-details/:id" element={<ProductDetails />} />
         <Route path="/success" element={<Success />} />
         <Route path="/cancel" element={<Cancel />} />
       </Route>
