6d020d2267bf3c2342c8a960d1b7e20ca0f6e882
rafael <rafaelmagalhaes1@live.com>
2024-05-13 11:50:33 -0300

    config vite file config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@chakra-ui/react', '@chakra-ui/icons', '@chakra-ui/theme-tools'],
+  },
 })
