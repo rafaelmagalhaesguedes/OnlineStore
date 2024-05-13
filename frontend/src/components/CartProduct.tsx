503634b52f4172cfa4e4258f39250df457b2d057
rafael <rafaelmagalhaes1@live.com>
2024-05-13 11:49:10 -0300

    refact: component CarProduct

--- frontend/src/components/CartProduct.tsx
+++ frontend/src/components/CartProduct.tsx
@@ -3,17 +3,19 @@ import { ProductType } from '../types/ProductType';
 import Button from 'react-bootstrap/Button';
 import { useContext } from 'react';
 
-export function CartProduct(props: { id: string, quantity?: number }) {
+type CartProductProps = {
+  product: ProductType;
+}
+
+export function CartProduct(props: CartProductProps) {
+  const { id, name, price, quantity } = props.product;
   const cart = useContext(CartContext);
-  const id = props.id;
-  const quantity = props.quantity || 0;
-  const productData = cart.getProductById(id) as ProductType;
 
   return (
     <>
-      <h3>{ productData.name }</h3>
+      <h3>{ name }</h3>
       <p>{ quantity } total</p>
-      <p>${ (quantity * productData.price).toFixed(2) }</p>
+      <p>${ (quantity * price).toFixed(2) }</p>
       <Button size="sm" onClick={() => cart.deleteFromCart(id)}>Remove</Button>
       <hr></hr>
     </>
